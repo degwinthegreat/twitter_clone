45b80a4229873830ff4627cb513b3a562d4ff8bf
Shinsuke Kawaida <the.colledge.dropout@gmail.com>
2018-03-10 04:52:35 +0000

    complete

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,3 @@
 class Tweet < ApplicationRecord
-    validates :content, presence: true
-    validates :content, length: { minimum: 1 }
-    validates :content, length: { maximum: 140 }
+    validates :content, presence: true,length: { maximum: 140 }
 end
