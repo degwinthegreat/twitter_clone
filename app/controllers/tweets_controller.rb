39178054b7cb12e6ec033a50dcff821114faaa74
kawaidashinsuke <the.colledge.dropout@gmail.com>
2018-03-17 16:51:28 +0900

    test

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,11 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only:[:show, :edit, :update, :destroy]
-  
+  before_action :login_check, only:[:new, :edit, :show]
+
   def index
     @tweets = Tweet.all
   end
-  
+
   def new
     if params[:back]
       @tweet = Tweet.new(tweet_params)
@@ -12,8 +13,8 @@ class TweetsController < ApplicationController
       @tweet = Tweet.new
     end
   end
-  
-  
+
+
   def create
     @tweet = Tweet.new(tweet_params)
     if @tweet.save
@@ -22,18 +23,18 @@ class TweetsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def home
     @tweet = Tweet.order(:content)
     @tweet = Tweet.limit(10).order(:content)
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
     if @tweet.update(tweet_params)
       redirect_to tweets_path, notice: "つぶやきを編集しました！"
@@ -41,12 +42,12 @@ class TweetsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @tweet.destroy
     redirect_to tweets_path, notice:"つぶやきを削除しました！"
   end
-  
+
   def confirm
     @tweet = Tweet.new(tweet_params)
     render :new if @tweet.invalid?
@@ -57,8 +58,14 @@ class TweetsController < ApplicationController
   def tweet_params
     params.require(:tweet).permit(:content)
   end
-  
+
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def login_check
+    if @currnet_user.nil?
+      redirect_to new_session_path
+    end
   end
 end
