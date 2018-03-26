cf8a81f95d488f0e65e39b498614c8b7c4fc0034
kawaidashinsuke <the.colledge.dropout@gmail.com>
2018-03-26 21:34:16 +0900

    test

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to: "current_user.email", subject:"お問い合わせの確認メール"
+    mail to: current_user.email, subject:"お問い合わせの確認メール"
   end
 
   default from: 'from@example.com'
